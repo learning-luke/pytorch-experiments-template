b9caaf7206cf8c62b166857b8cf92a3db9aabf84
Jack Turner <jackwilliamturner@icloud.com>
2021-03-05 16:40:52 +0000

    minor fixes

--- utils/data_loaders.py
+++ utils/data_loaders.py
@@ -12,7 +12,7 @@ class MNISTLoader:
     def __init__(self):
         normalize = transforms.Normalize(mean=[0.1307],
                                          std=[0.3081])
-        self.im_size = ImageShape(1, 28, 28)
+        self.image_shape = ImageShape(1, 28, 28)
 
         self.transform_train = transforms.Compose([
             transforms.ToTensor(),
@@ -36,9 +36,9 @@ class CINIC10Loader:
     def __init__(self):
         normalize = transforms.Normalize(mean=[0.4914, 0.4822, 0.4465],
                                          std=[0.2023, 0.1994, 0.2010])
-        self.im_size = ImageShape(3, 32, 32)
+        self.image_shape = ImageShape(3, 32, 32)
         self.transform_train = transforms.Compose([
-            transforms.RandomCrop(self.im_size.width, padding=4),
+            transforms.RandomCrop(self.image_shape.width, padding=4),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             normalize,
@@ -49,11 +49,14 @@ class CINIC10Loader:
             normalize,
         ])
 
-    def get_data(self, data_loc, download=False):
-        if download:
-            download_cinic(root_to_cinic.replace('-enlarged',''))
-            if '-enlarged' in which_set:
-                enlarge_cinic_10(root_to_cinic.replace('-enlarged',''))
+    def get_data(self, data_loc, download=False, enlarge=False):
+        if download and not os.path.exists(data_loc):
+            import ssl
+            ssl._create_default_https_context = ssl._create_unverified_context
+
+            download_cinic(data_loc.replace('-enlarged',''))
+            if enlarge:
+                enlarge_cinic_10(data_loc.replace('-enlarged',''))
 
         traindir = os.path.join(data_loc, 'train')
         valdir = os.path.join(data_loc, 'test')
@@ -68,9 +71,9 @@ class CIFAR10Loader:
     def __init__(self):
         normalize = transforms.Normalize(mean=[0.4914, 0.4822, 0.4465],
                                          std=[0.2023, 0.1994, 0.2010])
-        self.im_size = ImageShape(3, 32, 32)
+        self.image_shape = ImageShape(3, 32, 32)
         self.transform_train = transforms.Compose([
-            transforms.RandomCrop(self.im_size.width, padding=4),
+            transforms.RandomCrop(self.image_shape.width, padding=4),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             normalize,
@@ -94,9 +97,9 @@ class CIFAR100Loader:
     def __init__(self):
         normalize = transforms.Normalize(mean=[0.5071, 0.4866, 0.4409],
                                          std=[0.2009, 0.1984, 0.2023])
-        self.im_size = ImageShape(3, 32, 32)
+        self.image_shape = ImageShape(3, 32, 32)
         self.transform_train = transforms.Compose([
-            transforms.RandomCrop(self.im_size.width, padding=4),
+            transforms.RandomCrop(self.image_shape.width, padding=4),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             normalize,
@@ -120,9 +123,9 @@ class ImageNetLoader:
     def __init__(self):
         normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                          std=[0.229, 0.224, 0.225])
-        self.im_size = ImageShape(3, 224, 224)
+        self.image_shape = ImageShape(3, 224, 224)
         self.transform_train = transforms.Compose([
-            transforms.RandomResizedCrop(self.im_size.width),
+            transforms.RandomResizedCrop(self.image_shape.width),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             normalize,
@@ -144,7 +147,7 @@ class ImageNetLoader:
 
         return train_set, val_set
 
-def load_dataset(dataset, data_loc, batch_size=128, num_dataloading_workers=0, download=False):
+def load_dataset(dataset, data_loc, batch_size=128, num_workers=0, download=False):
 
     datasets = {
         'mnist': MNISTLoader,
@@ -157,18 +160,18 @@ def load_dataset(dataset, data_loc, batch_size=128, num_dataloading_workers=0, d
     dataloader = datasets[dataset.lower()]()
 
     ### e.g. ADD SIMCLR
-    dataloader.transform_train = SimCLRTransform(size=dataloader.im_size.width)
+    #dataloader.transform_train = SimCLRTransform(size=dataloader.im_size.width)
     ### 
 
     train_set, val_set = dataloader.get_data(data_loc, download)
 
     train_loader = torch.utils.data.DataLoader(
         train_set, batch_size=batch_size, shuffle=True,
-        num_dataloading_workers=num_dataloading_workers, pin_memory=True)
+        num_workers=num_dataloading_workers, pin_memory=True)
 
     val_loader = torch.utils.data.DataLoader(
         val_set,
         batch_size=batch_size, shuffle=False,
-        num_dataloading_workers=num_dataloading_workers, pin_memory=True)
+        num_workers=num_workers, pin_memory=True)
 
-    return train_loader, val_loader, dataloader.im_size
+    return train_loader, val_loader, train_set, val_set, dataloader.image_shape
