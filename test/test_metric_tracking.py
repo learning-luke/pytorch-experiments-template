495cf27669b915d7cb380ca5d6ce115ca7d9b96d
Jack Turner <jackwilliamturner@icloud.com>
2021-04-06 17:27:09 +0200

    Add some basic tests
    Former-commit-id: 053e4a28f49749db245a0cf37bde3a2fcc91bbd6

---
+++ test/test_metric_tracking.py
@@ -0,0 +1,58 @@
+import torch
+
+torch.manual_seed(0)
+
+
+def test_compute_accuracy():
+    """
+    def compute_accuracy(logits, targets):
+        acc = (targets == logits.argmax(-1)).float().detach().cpu().numpy()
+        return np.mean(acc)
+    """
+    from utils.metric_tracking import compute_accuracy
+
+    batch_size = 128
+    n_classes = 1000
+    logits = torch.randint(low=0, high=batch_size, size=(batch_size, n_classes))
+    targets = torch.zeros(batch_size)
+
+    for i, logit in enumerate(logits):
+        targets[i] = torch.argmax(logit)
+
+    assert compute_accuracy(logits, targets) == 1.0
+
+
+def test_push():
+    """
+    def push(self, epoch, iteration, logits, targets):
+        self.metrics["epochs"].append(epoch)
+        self.metrics["iterations"].append(iteration)
+        for k, fnc in self.metrics_to_track.items():
+            self.metrics[k].append(fnc(logits, targets))
+    """
+    pass
+
+
+def test_get_current_iteration_metric_trace_string():
+    """
+    def get_current_iteration_metric_trace_string(self):
+        return "".join(
+            [
+                (
+                    f"{key}: {value[-1]:0.4f}; "
+                    if (key != "epochs" and key != "iterations")
+                    else ""
+                )
+                for key, value in self.metrics.items()
+            ]
+        ).replace("(", "")
+    """
+    pass
+
+
+def test_metric_tracker():
+    """
+    A neat test to do here would be to feed a sequence of predictions to the
+    metric tracker and verify that the histories are correct.
+    """
+    pass
